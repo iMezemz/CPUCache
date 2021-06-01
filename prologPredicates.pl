eba29553c23e3691016ca29ccc6b5ef2941a83e0
OmarMelouk <okmelouk@gmail.com>
2021-06-01 23:48:46 +0200

    predicate BintoDec

--- prologPredicates.pl
+++ prologPredicates.pl
@@ -1,1 +1,13 @@
-%prolog predicates file
+
+%convertBinToDec(Bin, Dec).
+conHelper(0, _ , 0).
+conHelper(Bin, Multi, Dec):- X = Bin mod 10,
+							 D1 = (X * 2^Multi) ,
+							 Multi1 = (Multi + 1),
+							 Bin1 = (Bin // 10),
+							 Dec1 = (Dec + D1),
+							 conHelper(Bin1, Multi1, Dec1).
+
+
+
+convertBinToDec(Bin, Dec):- conHelper(Bin, 0, Dec).
